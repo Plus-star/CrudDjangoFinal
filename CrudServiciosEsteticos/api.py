7da1b03a141108cce8a821d90d188e0ba901776c
unknown <Sena@MEDAPRCSGFSD782.FormacionSena.RED>
2024-12-03 08:10:30 -0500

    git definitivo ahora si del todo

--- CrudServiciosEsteticos/api.py
+++ CrudServiciosEsteticos/api.py
@@ -1,8 +1,23 @@
 from .models import ServiciosEsteticos
+from .models import TratamientoFacial
+from .models import TratamientoCorporal
+
 from rest_framework import viewsets, permissions
 from .serializers import ServiciosEsteticosSerializer
+from .serializers import TratamientoFacialSerializer
+from .serializers import TratamientoCorporalSerializer
 
 class ServiciosEsteticosViewSet(viewsets.ModelViewSet):
     queryset = ServiciosEsteticos.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = ServiciosEsteticosSerializer
+    
+class TratamientoFacialViewSet(viewsets.ModelViewSet):
+    queryset = TratamientoFacial.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = TratamientoFacialSerializer
+    
+class TratamientoCorporalViewSet(viewsets.ModelViewSet):
+    queryset = TratamientoCorporal.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = TratamientoCorporalSerializer
